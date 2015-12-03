bc0f125f4177a0c55b8e06a6b48b5a5c3f7d5216
Davide Tampellini <tampe125@gmail.com>
2015-12-03 17:34:31 +0100

    Parse query string

--- drinkpy.py
+++ drinkpy.py
@@ -1,5 +1,6 @@
 import BaseHTTPServer
 import socket
+import urlparse
 
 __author__ = 'tampe125'
 
@@ -13,17 +14,18 @@ PORT_NUMBER = 9000
 
 
 class DrinkServer(BaseHTTPServer.BaseHTTPRequestHandler):
-    def do_GET(s):
+    def do_GET(self):
         """Respond to a GET request."""
-        s.send_response(200)
-        s.send_header("Content-type", "text/html")
-        s.end_headers()
-        s.wfile.write("<html><head><title>Title goes here.</title></head>")
-        s.wfile.write("<body><p>This is a test.</p>")
-        # If someone went to "http://something.somewhere.net/foo/bar/",
-        # then s.path equals "/foo/bar/".
-        s.wfile.write("<p>You accessed path: %s</p>" % s.path)
-        s.wfile.write("</body></html>")
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write("<html><head><title>Title goes here.</title></head>")
+        self.wfile.write("<body><p>This is a test.</p>")
+
+        qs = urlparse.parse_qs(urlparse.urlparse(self.path).query)
+
+        self.wfile.write("<p>You accessed path: %s</p>" % qs)
+        self.wfile.write("</body></html>")
 
 
 if __name__ == '__main__':
