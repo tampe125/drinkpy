be2fcc45979f80ade11dc5f672adce3e927d6ef8
Davide Tampellini <tampe125@gmail.com>
2015-12-06 21:48:08 +0100

    Suppressed standard logging Provide the result as JSON

--- drinkpy.py
+++ drinkpy.py
@@ -5,7 +5,7 @@ import urlparse
 __author__ = 'tampe125'
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.connect(("8.8.8.8",80))
+s.connect(("8.8.8.8", 80))
 ip = (s.getsockname()[0])
 s.close()
 
@@ -17,15 +17,16 @@ class DrinkServer(BaseHTTPServer.BaseHTTPRequestHandler):
     def do_GET(self):
         """Respond to a GET request."""
         self.send_response(200)
-        self.send_header("Content-type", "text/html")
+        self.send_header("Content-type", "application/json")
         self.end_headers()
-        self.wfile.write("<html><head><title>Title goes here.</title></head>")
-        self.wfile.write("<body><p>This is a test.</p>")
 
         qs = urlparse.parse_qs(urlparse.urlparse(self.path).query)
 
-        self.wfile.write("<p>You accessed path: %s</p>" % qs)
-        self.wfile.write("</body></html>")
+        self.wfile.write(qs)
+
+    def log_message(self, format, *args):
+        """Override standard logging to prevent too much output in console"""
+        pass
 
 
 if __name__ == '__main__':
