dce1c7ef5248149cf736892c50f3879d3881133e
Davide Tampellini <tampe125@gmail.com>
2015-12-09 21:48:23 +0100

    Added some fake method

--- lib/FakeHat.py
+++ lib/FakeHat.py
@@ -2,4 +2,16 @@
 
 class FakeHat:
     def __init__(self):
+        self.num = 0
         pass
+
+    def getMotor(self, num):
+        self.num = num
+
+        return self
+
+    def run(self, command):
+        print "Motor " + str(self.num) + " set command: " + str(command)
+
+    def setSpeed(self, speed):
+        print "Motor " + str(self.num) + " set speed: " + str(speed)
